ab841ac128c80bf51651a2055e7de7d2f17627d1
xomaxy <50502003+xomaxy@users.noreply.github.com>
2024-04-25 12:41:14 -0600

    Update functools.py

--- functools.py
+++ functools.py
@@ -5,18 +5,19 @@ def generar_secuencia(n):
     return ''.join(np.random.choice(amino_set, n))
 
 def cambiar_letras_aleatoriamente(secuencia, n_cambios):
+    amino_set = ['A', 'M', 'B', 'N', 'C', 'P', 'D', 'Q', 'E', 'R', 'F', 'S', 'G', 'T', 'H', 'V', 'I', 'W', 'K', 'Y', 'L', 'Z']
     # Convert the sequence into a list to allow modifications
     secuencia_lista = list(secuencia)
     longitud_secuencia = len(secuencia_lista)
-    
+
     # Perform n_cambios random changes in the sequence
     for _ in range(n_cambios):
         # Select a random position to change
         pos = np.random.randint(longitud_secuencia)
         # Randomly select a new letter from the array
-        nueva_letra = np.random.choice(letras_mayusculas)
+        nueva_letra = np.random.choice(amino_set)
         # Replace the letter at the chosen position
         secuencia_lista[pos] = nueva_letra
-    
+
     # Convert the list back to a string
     return ''.join(secuencia_lista)
